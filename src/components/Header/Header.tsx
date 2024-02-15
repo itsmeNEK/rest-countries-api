ae208582f00b3c156fec318adc04825bca5776e9
itsmeNEK <gasparkenken55@gmail.com>
2024-02-15 19:39:07 +0800

    feat: CountryCard
    Add country card that will display the flag name population region and capital of a country. Card is clickable that will redirect to Country Page. Country page is not yet created

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Link from 'next/link'
 import PrimaryButton from '../common/button/PrimaryButton'
 import MoonSvgIcon from '../common/svg/MoonSvgIcon'
 import Style from './Header.module.scss'
@@ -13,7 +14,9 @@ export default function Header() {
   return (
     <nav className={Style['header']}>
       <div className={`wrapper ${Style['wrapper']}`}>
-        <h1 className={Style['header__brand']}>Where in the world?</h1>
+        <Link href='/'>
+          <h1 className={Style['header__brand']}>Where in the world?</h1>
+        </Link>
         <PrimaryButton
           className={Style['header__button']}
           type='button'
