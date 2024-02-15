eb5a9c8e5103fadb6b94c786ed1bc0b36346434c
BILL ZHEDRICK GASPAR <88978837+itsmeNEK@users.noreply.github.com>
2024-02-15 21:07:43 +0800

    Feature/country cards (#6)
    * feat: CountryCard
    
    Add country card that will display the flag name population region and capital of a country. Card is clickable that will redirect to Country Page. Country page is not yet created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import CountryCard from '@/components/CountryCard/CountryCard'
+import { CountryTypes } from '@/types/countryTypes'
+
 export default function Home() {
+  const country: CountryTypes = {
+    name: 'Afghanistan',
+    alpha3Code: 'AFG',
+    capital: 'Kabul',
+    region: 'Asia',
+    population: 40218234,
+    flags: {
+      svg: 'https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_the_Taliban.svg',
+      png: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_the_Taliban.svg/320px-Flag_of_the_Taliban.svg.png',
+    },
+  }
   return (
     <main>
-      <p>REST Countries API</p>
+      <CountryCard country={country} />
     </main>
   )
 }
---
+++ src/types/countryTypes.tsx
@@ -0,0 +1,11 @@
+export type CountryTypes = {
+  name: string
+  population: number
+  alpha3Code: string
+  region: string
+  capital: string
+  flags: {
+    svg: string
+    png: string
+  }
+}
